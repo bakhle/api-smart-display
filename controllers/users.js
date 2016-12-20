22bdebb7c9c81b835570f2bfed5b7bae03ff730e
Bhargavi Bakhle <bhargavi.bakhle15@gmail.com>
2016-12-20 23:29:35 +0530

    Stocks Done

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const _ = require('lodash');
 const request = require('request');
 
 const config = require('../config');
@@ -10,6 +11,7 @@ const userProfile = config.getUserProfile();
 
 // change news source as needed
 const newsApiBaseUrl = 'https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=' + newsApiKey;
+const stocksApiBaseUrl = 'http://www.google.com/finance/info?q=';
 
 
 module.exports.returnNews = (req, res) => {
@@ -31,7 +33,28 @@ module.exports.returnPersonalGreetings = (req, res) => {
 
 
 module.exports.returnStocks = (req, res) => {
-  res.json(stocks);
+
+  let requestUrl = stocksApiBaseUrl;
+
+  // add all specified stocks to url
+  for(let key in stocks) {
+    requestUrl += stocks[key] + ',';
+  };
+
+  request(requestUrl, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return res.status(500).end();
+    };
+
+    // returns json but 1st two chars are `//`
+    // slice them off to parse the json
+    body = _.trim(body);
+    body = _.replace(body, '//', '' );
+    body = JSON.parse(body);
+
+    res.contentType("JSON").json(body);
+  })
 };
 
 
