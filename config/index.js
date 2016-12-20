14a12e9bb17c00e507bccda695ad678d1b704ba7
Bhargavi Bakhle <bhargavi.bakhle15@gmail.com>
2016-12-20 22:21:38 +0530

    News Done

--- config/index.js
+++ config/index.js
@@ -1,11 +1,15 @@
 'use strict'
 
-const userProfile = require('./profile');
+const config = require('./config')
 const personalGreetings = require('./greetings');
 const stocks = require('./stocks');
+const userProfile = require('./profile');
 
 
 module.exports = {
+  getNewsApiKey : function () {
+    return config.apiKeys.newsApiOrg;
+  },
   getPersonalGreetings : function () {
     return personalGreetings;
   },
--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,27 @@
 'use strict'
 
+const request = require('request');
+
 const config = require('../config');
-const userProfile = config.getUserProfile();
+const newsApiKey = config.getNewsApiKey();
 const personalGreetings = config.getPersonalGreetings();
 const stocks = config.getStocks();
+const userProfile = config.getUserProfile();
+
+// change news source as needed
+const newsApiBaseUrl = 'https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=' + newsApiKey;
 
 
 module.exports.returnNews = (req, res) => {
-  res.json({News: "News Headlines"});
+
+  request(newsApiBaseUrl, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return res.status(500).end();
+    };
+    body = JSON.parse(body);
+    res.json(body.articles);
+  })
 };
 
 
