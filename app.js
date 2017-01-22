d8d324dcf9e1a529d319a93fe9896a05564deb4f
Bhargavi Bakhle <bhargavi.bakhle15@gmail.com>
2017-01-22 18:47:25 +0530

    Allow CORS

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,20 @@ const router = express.Router();
 
 const userController = require('../controllers/users');
 
+// middleware to allow CORS
+router.use(function(req, res, next) {
+  var allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
+  var origin = req.headers.origin;
+
+  if(allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 router.get('/', userController.returnUserProfile);
 
 router.get('/greetings', userController.returnPersonalGreetings);
