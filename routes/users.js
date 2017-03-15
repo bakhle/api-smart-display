106c5f636a33b314393f42e69a78045f87470e9c
aarongoa <taipan93@gmail.com>
2017-03-16 00:12:51 +0530

    - Allow CORS from any origin - Allow DELETE requests as well

--- routes/users.js
+++ routes/users.js
@@ -11,10 +11,10 @@ router.use(function(req, res, next) {
   var allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
   var origin = req.headers.origin;
 
-  if(allowedOrigins.indexOf(origin) > -1) {
+  // if(allowedOrigins.indexOf(origin) > -1) {
     res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  // }
+  res.header("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
   next();
